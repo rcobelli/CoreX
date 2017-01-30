d749e9714ab51eed8b6d90099d0a6bbfb79ca443
Ryan Cobelli <rybelllc@gmail.com>
2017-01-29 23:15:01 -0500

    Fix health data (T191)

--- core-x/HealthManager.swift
+++ core-x/HealthManager.swift
@@ -13,11 +13,11 @@ class HealthManager {
   let healthKitStore:HKHealthStore = HKHealthStore()
   
 
-  func authorizeHealthKit(completion: ((success:Bool, error:NSError!) -> Void)!) {
+  func authorizeHealthKit() {
 
     // 2. Set the types you want to write to HK Store
     let healthKitTypesToWrite = Set(arrayLiteral:
-      HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierActiveEnergyBurned)!,
+      HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!,
       HKQuantityType.workoutType()
       )
 
@@ -25,26 +25,24 @@ class HealthManager {
     if !HKHealthStore.isHealthDataAvailable()
     {
       let error = NSError(domain: "com.raywenderlich.tutorials.healthkit", code: 2, userInfo: [NSLocalizedDescriptionKey:"HealthKit is not available in this Device"])
-      if( completion != nil )
-      {
-        completion(success:false, error:error)
-      }
-      return;
+      print(error)
     }
 
     // 4.  Request HealthKit authorization
-    healthKitStore.requestAuthorizationToShareTypes(healthKitTypesToWrite, readTypes: nil) { (success, error) -> Void in
-
-      if( completion != nil )
-      {
-        completion(success:success,error:error)
-      }
-    }
+	healthKitStore.requestAuthorization(toShare: healthKitTypesToWrite, read: nil) { (success, error) -> Void in
+		if success {
+			print("Authorized")
+		} else {
+			print("Denied")
+		}
+		
+		if let error = error { print(error) }
+	}
   }
 
 
-  
-	func saveWorkout(duration:Double, workoutNumber: Int, completion: ( (Bool, NSError!) -> Void)!) {
+	
+	func saveWorkout(_ duration:Double, workoutNumber: Int, completion: ( (Bool, NSError?) -> Void)!) {
 		
 		var calories = 0.0
 		var workoutName = ""
@@ -53,37 +51,40 @@ class HealthManager {
 		switch workoutNumber {
 		case 0:
 			workoutName = "Core X"
-			calories = 10.0 * (duration * 8.0)
+			calories = 10.0 * ((duration / 30.0) * 8.0)
 			totalDuration = duration * 10.0
 		case 1:
 			workoutName = "Myrtl"
-			calories = 14.0 * (duration * 4.0)
+			calories = 14.0 * ((duration / 20.0) * 4.0)
 			totalDuration = duration * 14.0
 		case 2:
 			workoutName = "Leg-Day"
-			calories = 9.0 * (duration * 2.0)
+			calories = 9.0 * ((duration / 30.0) * 2.0)
 			totalDuration = duration * 9.0
 		case 3:
 			workoutName = "101 Pushups"
-			calories = 20.0 * (duration * 20.0)
+			calories = 20.0 * ((duration / 20.0) * 20.0)
 			totalDuration = duration * 20.0
 		case 4:
 			workoutName = "Yogata Be Kidding Me"
-			calories = 11.0 * (duration * 2.0)
+			calories = 11.0 * ((duration / 25.0) * 2.0)
 			totalDuration = duration * 11.0
 		default:
 			workoutName = "Coach Liz Stretch Routine"
-			calories = 6.0 * (duration * 2.0)
+			calories = 6.0 * ((duration / 25.0) * 2.0)
 			totalDuration = duration * 8.0
 		}
 	
-      let caloriesQuantity = HKQuantity(unit: HKUnit.kilocalorieUnit(), doubleValue: calories)
+		calories = calories / 1000.0
+        let caloriesQuantity = HKQuantity(unit: HKUnit.kilocalorie(), doubleValue: calories)
       
-      // 2. Save Running Workout
-	let workout = HKWorkout(activityType: HKWorkoutActivityType.CrossTraining, startDate: NSDate().dateByAddingTimeInterval(-totalDuration), endDate: NSDate(), duration: totalDuration, totalEnergyBurned: caloriesQuantity, totalDistance: HKQuantity(unit: HKUnit.mileUnit(), doubleValue: 0.0), metadata: ["Workout Name": workoutName])
-	healthKitStore.saveObject(workout, withCompletion: { (success, error) -> Void in
-		completion(success,error)
-	})
-  }
+		// 2. Save Running Workout
+		let workout = HKWorkout(activityType: HKWorkoutActivityType.crossTraining, start: Date().addingTimeInterval(-totalDuration), end: Date(), duration: totalDuration, totalEnergyBurned: caloriesQuantity, totalDistance: HKQuantity(unit: HKUnit.mile(), doubleValue: 0.0), metadata: ["Workout Name": workoutName])
+		healthKitStore.save(workout, withCompletion: { (success, error) -> Void in
+			if !success {
+				print(error!)
+			}
+		})
+	}
   
 }
